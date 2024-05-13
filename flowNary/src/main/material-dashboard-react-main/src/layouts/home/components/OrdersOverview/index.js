25d70d5e243a5a5bd156c3e5d6e0fa7242b8b4a6
develop_yunju <ilove4903@naver.com>
2024-05-13 19:13:07 +0900

    [fix] Home
    게시글에 임의 글자 입력

--- flowNary/src/main/material-dashboard-react-main/src/layouts/home/components/OrdersOverview/index.js
+++ flowNary/src/main/material-dashboard-react-main/src/layouts/home/components/OrdersOverview/index.js
@@ -19,7 +19,7 @@ import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
+import MDTypography from "components/MDTypography";  
 
 // Material Dashboard 2 React example components
 import TimelineItem from "examples/Timeline/TimelineItem";
