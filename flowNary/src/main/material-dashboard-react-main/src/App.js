a641ec0038e6833b0d2a7d122332b42214c169d7
develop_yunju <ilove4903@naver.com>
2024-05-14 19:27:32 +0900

    react-Final Project

--- flowNary/src/main/material-dashboard-react-main/src/App.js
+++ flowNary/src/main/material-dashboard-react-main/src/App.js
@@ -16,7 +16,7 @@ Coded by www.creative-tim.com
 import { useState, useEffect, useMemo } from "react";
 
 // react-router components
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";  
 
 // @mui material components
 import { ThemeProvider } from "@mui/material/styles";
