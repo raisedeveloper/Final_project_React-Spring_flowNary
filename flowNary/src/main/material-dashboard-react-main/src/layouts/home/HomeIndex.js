c2e53e60944ff20c59e07258388b200e79c156cd
Juyoung Kwak <rmmr123@naver.com>
2024-05-27 21:07:17 +0900

    좋아요 처리

--- flowNary/src/main/material-dashboard-react-main/src/layouts/home/HomeIndex.js
+++ flowNary/src/main/material-dashboard-react-main/src/layouts/home/HomeIndex.js
@@ -3,25 +3,36 @@ import Grid from "@mui/material/Grid";
 import axios from "axios";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
+import CloseIcon from '@mui/icons-material/Close';
+
 
 // Dashboard components
 import TodoList from "layouts/home/components/todoList";
 import { Avatar, Box, Button, Card, CardContent, CardHeader, CardMedia, Divider, Icon, IconButton, Modal, Stack, Typography, } from "@mui/material";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Popover from '@mui/material/Popover';
 
+
+
+import { alpha, useTheme, styled } from '@mui/material/styles';
 import { Bar } from "react-chartjs-2";
 import MDTypography from "components/MDTypography";
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useLayoutEffect, useRef, useState } from "react";
 import BoardDetail from "./Board/BoardDetail";
 import Write from './write';
 
 import { useLocation, useNavigate } from "react-router-dom";
-import { GetWithExpiry } from "api/LocalStorage";
+import { UserContext, GetWithExpiry } from "api/LocalStorage";
 import { useAddLike, useGetUserNicknameLS } from "api/customHook";
 import { useQuery, useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 import { getBoardList, getBoardListCount } from "api/axiosGet";
@@ -32,6 +43,10 @@ import koreanStrings from './Board/ko'; // 한글 로케일 파일 경로
 export default function Home() {
   const queryClient = useQueryClient()
   const [expanded, setExpanded] = useState({});
+
+  // const Transition = React.forwardRef(function Transition(props, ref) {
+  //   return <Slide direction="right" ref={ref} {...props} />;
+  // });
 
   const handleToggle = (bid) => {
     setExpanded((prevExpanded) => ({
@@ -133,7 +148,6 @@ export default function Home() {
       const res = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lang=kr&lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
       );
-      console.log(res);
       // id 찾아서 매칭 후 description 한글 번역된 거 가져오기
       const weatherId = res.data.weather[0].id;
       const weatherKo = weatherDescKo[weatherId];
@@ -170,6 +184,7 @@ export default function Home() {
   const profile = GetWithExpiry("profile");
 
   const [bid, setBid] = useState(0);
+  const [index, setIndex] = useState(0);
   const [text, setText] = useState('');
   const [open, setOpen] = useState(false);
 
@@ -178,14 +193,26 @@ export default function Home() {
   // }
   const nickname = useGetUserNicknameLS();
 
+
+  //수정 페이지 이동
+  const handleUpdate = () => {
+    navigate("../home/Update")
+    sessionStorage.setItem("bid", bid);
+  }
+
+  const { activeUser } = useContext(UserContext);
+
+
   // 창 열고 닫기
-  const handleOpen = (e) => {
+  const handleOpen = (e, idx) => {
+
     setOpen(true);
     setBid(e);
+    setIndex(idx);
   }
   const handleClose = () => {
     setOpen(false);
-    setBid(-1);
+    console.log(open)
   };
 
   const location = useLocation();
@@ -203,7 +230,6 @@ export default function Home() {
     queryKey: ['boardList', uid],
     queryFn: ({ pageParam = 1 }) => getBoardList(pageParam * count, uid),
     getNextPageParam: (lastPage) => {
-      console.log("lastPage:", lastPage);
       if (lastPage && lastPage.nextCursor !== undefined) {
         return lastPage.nextCursor;
       }
@@ -298,8 +324,61 @@ export default function Home() {
 
     addLikeForm(sendData);
   }
+  // 댓글 좋아요 버튼 누를 때 넘기기
+  function handleButtonLikeReply(rid, uid2) {
+    if (uid == -1)
+      return;
+
+    const sendData = {
+      uid: activeUser.uid,
+      fuid: uid2,
+      oid: rid,
+      type: 2,
+    }
+
+    addLikeForm(sendData);
+  }
+  // 대댓글 좋아요 버튼 누를 때 넘기기
+  function handleButtonLikeReReply(rrid, uid2) {
+    if (uid === -1) return;
+
+    const sendData = {
+      uid: activeUser.uid,
+      fuid: uid2,
+      oid: rrid,
+      type: 3,
+    }
+
+    addLikeForm(sendData);
+  }
   if (isLoading) {
     <div>로딩중...</div>
+  }
+
+
+  //popover
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClosePopover = () => {
+    setAnchorEl(null);
+  };
+
+  const openPopover = Boolean(anchorEl);
+  const id = openPopover ? 'simple-popover' : undefined;
+
+  // 클릭 시 마이페이지 이동 이벤트
+  const handleMyPage = (uid) => {
+    navigate("/mypage", { state: { uid: uid } }); // state를 통해 navigate 위치에 파라메터 제공
+  }
+
+  if (isLoading) {
+    return (
+      <div>Loading</div>
+    )
   }
 
   return (
@@ -314,7 +393,7 @@ export default function Home() {
                 {(boardList && allcount && !isLoading) ? (boardList.pages.map((page, index) => (
                   <React.Fragment key={index}>
                     {page.map((data, idx) => (
-                      <Grid key={idx} item xs={12} md={6} lg={6}>
+                      <Grid key={idx} item xs={12} md={6} lg={6} >
                         <MDBox mb={3}>
                           <Card sx={{
                             height: "100%",
@@ -332,14 +411,48 @@ export default function Home() {
                                 />
                               }
                               action={
-                                <IconButton aria-label="settings">
-                                  <MoreVertIcon />
-                                </IconButton>
+                                <div>
+                                  <IconButton aria-label="settings" onClick={handleClick}>
+                                    <MoreVertIcon />
+                                  </IconButton>
+                                  <Popover
+                                    id={id}
+                                    open={openPopover}
+                                    anchorEl={anchorEl}
+                                    onClose={handleClosePopover}
+                                    anchorOrigin={{
+                                      vertical: 'bottom',
+                                      horizontal: 'left',
+                                    }}
+                                    transformOrigin={{
+                                      vertical: 'top',
+                                      horizontal: 'right',
+                                    }}
+                                    PaperProps={{
+                                      style: {
+                                        marginLeft: 90, // 이 값을 조정하여 팝오버의 가로 위치를 미세하게 조정
+                                      },
+                                    }}
+                                  >
+                                    {data.uid === activeUser.uid && (
+                                      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+
+                                        <Button onClick={handleUpdate}>수정</Button>
+                                        <Button sx={{ padding: 0 }}>공유 하기</Button>
+                                        <Button sx={{ padding: 0, color: 'red' }}>신고 하기</Button>
+                                        <Button sx={{ padding: 0 }}>삭제 하기</Button>
+
+                                      </Box>
+                                    )}
+                                  </Popover>
+                                </div>
                               }
                               title={<Typography variant="subtitle3" sx={{ fontSize: "15px", color: 'purple' }}>{data.nickname}</Typography>}
+                              // 클릭 시 마이페이지 이동 이벤트
+                              onClick={() => handleMyPage(data.uid)}
                             />
 
-                            <MDBox padding="1rem">
+                            <MDBox padding="1rem" onClick={handleOpen.bind(null, data.bid, idx)}>
                               {data.image ?
                                 <MDBox
                                   variant="gradient"
@@ -360,13 +473,11 @@ export default function Home() {
                                     }
                                   }}
                                 >
-                                  <button onClick={handleOpen.bind(null, data.bid)}>
-                                    <img
-                                      src={data.image ? `https://res.cloudinary.com/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload/${data.image.split(',')[0]}` : ''}
-                                      alt="Paella dish"
-                                      style={{ cursor: 'pointer', width: '100%', height: '100%', objectFit: 'cover', position: 'absolute', top: 0, left: 0, borderRadius: 'inherit' }}
-                                    />
-                                  </button>
+                                  <img
+                                    src={data.image ? `https://res.cloudinary.com/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload/${data.image.split(',')[0]}` : ''}
+                                    alt="Paella dish"
+                                    style={{ cursor: 'pointer', width: '100%', height: '100%', objectFit: 'cover', position: 'absolute', top: 0, left: 0, borderRadius: 'inherit' }}
+                                  />
                                 </MDBox>
                                 :
                                 <MDBox>
@@ -389,31 +500,27 @@ export default function Home() {
                                       }
                                     }}
                                   >
-                                    <button onClick={handleOpen.bind(null, data.bid)}>
-                                      <img
-                                        src="images/LightLogo.png"
-                                        alt="Paella dish"
-                                        style={{ cursor: 'pointer', width: '100%', height: '100%', objectFit: 'fill', position: 'absolute', top: 0, left: 0, borderRadius: 'inherit' }}
-                                      />
-                                    </button>
+                                    <img
+                                      src="images/LightLogo.png"
+                                      alt="Paella dish"
+                                      style={{ cursor: 'pointer', width: '100%', height: '100%', objectFit: 'fill', position: 'absolute', top: 0, left: 0, borderRadius: 'inherit' }}
+                                    />
                                   </MDBox>
                                 </MDBox>
                               }
                               <MDBox pt={3} pb={1} px={1}>
-                                <button onClick={handleOpen.bind(null, data.bid)} style={{ border: 'none', backgroundColor: 'transparent', padding: 0, margin: 0 }}>
-                                  <MDTypography variant="h6" textTransform="capitalize">
-                                    {data.title}
+                                <MDTypography variant="h6" textTransform="capitalize">
+                                  {data.title}
+                                </MDTypography>
+                                {expanded[data.bid] ? (
+                                  <MDTypography component="div" variant="button" color="text" fontWeight="light">
+                                    {data.bContents}
                                   </MDTypography>
-                                  {expanded[data.bid] ? (
-                                    <MDTypography component="div" variant="button" color="text" fontWeight="light">
-                                      {data.bContents}
-                                    </MDTypography>
-                                  ) : (
-                                    <MDTypography component="div" variant="button" color="text" fontWeight="light" sx={{ display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical', overflow: 'hidden' }}>
-                                      {data.bContents}
-                                    </MDTypography>
-                                  )}
-                                </button>
+                                ) : (
+                                  <MDTypography component="div" variant="button" color="text" fontWeight="light" sx={{ display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical', overflow: 'hidden' }}>
+                                    {data.bContents}
+                                  </MDTypography>
+                                )}
                                 <Button onClick={() => handleToggle(data.bid)}>{expanded[data.bid] ? '...' : '...'}</Button>
                                 <Divider />
                                 <MDBox display="flex" alignItems="center">
@@ -444,6 +551,7 @@ export default function Home() {
                     <MDBox pt={3} px={3}>
                       <MDTypography variant="h6" fontWeight="medium">
                         Weather 정보
+                        {open === true ? 1 : 0}
                       </MDTypography>
                       <Avatar sx={{ width: 200, height: 200 }} src={weather.links} alt="날씨 아이콘" />
                       <CardContent sx={{ fontSize: 'large', fontWeight: 'bolder' }}>
@@ -460,15 +568,38 @@ export default function Home() {
             </Stack>
           </Stack>
         </MDBox >
-
       </MDBox >
       {/* <div ref={observerRef}></div> */}
       <div id="observe" ref={observerRef} style={{ display: 'flex', height: '1rem' }}></div>
 
       {/* 게시글 모달 */}
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-        <BoardDetail bid={bid} uid={uid} handleClose={handleClose} nickname={nickname} handleButtonLike={handleButtonLike} />
-      </Modal>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        // TransitionComponent={Transition}
+        aria-labelledby="customized-dialog-title"
+        keepMounted
+        PaperProps={{
+          style: {
+            width: '75vw', // 원하는 너비로 설정
+            maxWidth: '75vw', // 최대 너비 고정
+            height: '80vh', // 원하는 높이로 설정
+            maxHeight: '80vh', // 최대 높이 고정
+          },
+
+        }}
+      >
+        <IconButton aria-label="close" onClick={handleClose}
+          sx={{
+            position: 'absolute', right: 8, top: 8,
+            color: (theme) => theme.palette.grey[500],
+            zIndex: 2
+          }} >
+          <CloseIcon />
+        </IconButton>
+        <BoardDetail bid={bid} uid={uid} index={index} handleClose={handleClose} nickname={nickname} handleButtonLikeReply={handleButtonLikeReply} handleButtonLikeReReply={handleButtonLikeReReply} handleButtonLike={handleButtonLike} />
+      </Dialog>
+
       <Footer />
     </DashboardLayout >
   );
